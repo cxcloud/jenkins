553851644b54eff7eb9ce86c6abb90895f63a33d
Ilia Bazanov <ilia.bazanov@tieto.com>
2019-02-06 11:17:27 +0200

    Update detectModifiedProjects.sh

--- dockerfiles/cxcloud-worker/detectModifiedProjects.sh
+++ dockerfiles/cxcloud-worker/detectModifiedProjects.sh
@@ -1,24 +1,34 @@
 #!/bin/bash
 
+# global parameters
+# -----------------
+# $1 - file to search for
+# $2 - git hash fromP
+# $3 - git hash to
+
+# rfind() parameters
+# ------------------
 # $1 - file name to find
 # $2 - start path from where to start search
 
-function rfind () {
+function rfind() {
   current_folder="$2"
-
-  while [[ $current_folder != "." ]] ; do
+  while [[ $current_folder != "." ]]; do
     modified_projects+=($(find "$current_folder" -maxdepth 1 -name "$1"))
     current_folder=$(dirname $current_folder)
   done
 }
 
 modified_projects=()
-modified_files=$(git log master..HEAD --name-only --pretty=)
+
+if [ -z "$2" ]; then
+  modified_files=$(git log master..HEAD --name-only --pretty=)
+else
+  modified_files=$(git log $2..$3 --name-only --pretty=)
+fi
 
 while read -r line; do
-    rfind index.html $(dirname "${line}")
-done <<< "$modified_files"
+  rfind $1 $(dirname "${line}")
+done <<<"$modified_files"
 
-sorted_files=($(echo "${modified_projects[@]}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-
-echo ${sorted_files[@]}
+echo "${modified_projects[@]}" | tr ' ' '\n' | sort -u
